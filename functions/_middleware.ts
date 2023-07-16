4e38208c3b0dddb9c2c026853d7398e08afc1761
Tom Prins <thomas.prins@gropyus.com>
2023-07-16 17:21:12 +0200

    add some logging

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -8,12 +8,14 @@ export async function onRequest({ request, next, env: { CFP_PASSWORD } }: Cloudf
 
   const { error } = Object.fromEntries(requestUrl.searchParams);
   if (isString(error) || CFP_ALLOWED_PATHS.includes(requestUrl.pathname)) {
+    console.log(`moving on... ${error}`);
     return next();
   }
 
   const response = new Response('', { status: 302, headers: { 'Cache-Control': 'no-cache' } });
 
   if (!isString(CFP_PASSWORD)) {
+    console.log('missing password!', Errors.Missing.toString());
     requestUrl.searchParams.append('error', Errors.Missing.toString());
     response.headers.set('Location', requestUrl.toString());
     return response;
@@ -21,14 +23,21 @@ export async function onRequest({ request, next, env: { CFP_PASSWORD } }: Cloudf
 
   const cookie = request.headers.get('cookie');
 
-  if (isString(cookie)) {
-    const password = await sha256(CFP_PASSWORD);
-
-    if (cookie.includes(getCookie(password))) {
-      return next();
-    }
+  if (!isString(cookie)) {
+    console.log('empty password!', Errors.Empty.toString());
+    requestUrl.searchParams.append('error', Errors.Empty.toString());
+    response.headers.set('Location', requestUrl.toString());
+    return response;
   }
 
+  const password = await sha256(CFP_PASSWORD);
+
+  if (cookie.includes(getCookie(password))) {
+    console.log('MATCHES!!')
+    return next();
+  }
+
+  console.log('MISSING OR WRONG!');
   requestUrl.searchParams.append('error', Errors.Incorrect.toString());
   response.headers.set('Location', requestUrl.toString());
   return response;
--- utils/constants.ts
+++ utils/constants.ts
@@ -2,6 +2,7 @@ export enum Errors {
     Missing = 'missing-password',
     Incorrect = 'incorrect-password',
     Invalid = 'invalid-method',
+    Empty = 'empty-password',
 }
 
 export const CFP_COOKIE_KEY = 'CFP-Auth-Key';
