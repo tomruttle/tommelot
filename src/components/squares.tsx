baaef72fe6a6e6324619feabe4b89539c3c79f2e
Tom Prins <thomas.prins@gropyus.com>
2023-07-23 21:04:48 +0200

    rename t inclination

--- src/components/squares.tsx
+++ src/components/squares.tsx
@@ -3,24 +3,24 @@ import { randomNumber } from "../utils/shared";
 import { Square } from "./square";
 import { Tile } from "./tile";
 
-function getAngleIncrement(t: number, radius: number, tileSize: number) {
-  const currentRadius = Math.abs(2 * radius * Math.sin(t)) / 2.5;
+function getAngleIncrement(inclination: number, radius: number, tileSize: number) {
+  const currentRadius = Math.abs(2 * radius * Math.sin(inclination)) / 2.5;
   const circumference = Math.abs(MAX_ANGLE * currentRadius);
   const squaresThatFit = Math.floor(circumference / tileSize);
 
   return (MAX_ANGLE - FUZZY) / squaresThatFit;
 }
 
-function getSquareTransform(i: number, t: number, radius: number) {
-  const x = radius * Math.cos(i) * Math.sin(t);
-  const y = radius * Math.sin(i) * Math.sin(t);
-  const z = radius * Math.cos(t);
+function getSquareTransform(i: number, inclination: number, radius: number) {
+  const x = radius * Math.cos(i) * Math.sin(inclination);
+  const y = radius * Math.sin(i) * Math.sin(inclination);
+  const z = radius * Math.cos(inclination);
 
   return `translateX(${x}px) translateY(${y}px) translateZ(${z}px)`
 }
 
-function getTileBackgroundColour(t: number) {
-  const isBright = (t > 1.3 && t < 1.9) || (t < -1.3 && t > -1.9);
+function getTileBackgroundColour(inclination: number) {
+  const isBright = (inclination > 1.3 && inclination < 1.9) || (inclination < -1.3 && inclination > -1.9);
   const colourValue = isBright ? randomNumber(130, 255) : randomNumber(110, 190);
 
   return `rgb(${colourValue},${colourValue},${colourValue})`;
@@ -29,20 +29,20 @@ function getTileBackgroundColour(t: number) {
 export function Squares({ radius, tileSize, prec, reflectSpeed }: { radius: number, tileSize: number, prec: number, reflectSpeed: number }) {
   const squares: Array<JSX.Element> = []
 
-  for (let t = FUZZY; t < Math.PI; t += (Math.PI - FUZZY) / prec) {
-    const angleInc = getAngleIncrement(t, radius, tileSize);
+  for (let inclination = FUZZY; inclination < Math.PI; inclination += (Math.PI - FUZZY) / prec) {
+    const angleInc = getAngleIncrement(inclination, radius, tileSize);
 
     for (let i = (angleInc / 2) + FUZZY; i < MAX_ANGLE; i += angleInc) {
       squares.push((
         <Square
-          key={`t${t}i${i}`}
-          transform={getSquareTransform(i, t, radius)}
+          key={`inc${inclination}i${i}`}
+          transform={getSquareTransform(i, inclination, radius)}
           radius={radius}
         >
           <Tile
-            transform={`rotate(${i}rad) rotateY(${t}rad)`}
+            transform={`rotate(${i}rad) rotateY(${inclination}rad)`}
             animationDelay={randomNumber(0, 20) / 10}
-            backgroundColor={getTileBackgroundColour(t)}
+            backgroundColor={getTileBackgroundColour(inclination)}
             size={tileSize}
             reflectSpeed={reflectSpeed}
           />
